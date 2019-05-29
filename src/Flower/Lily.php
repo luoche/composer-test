f18ad47e9406bd0bf2cf7a2c1c484afcffa489fd
wangyafei <wangyafei@ecqun.com>
2019-05-29 23:49:14 +0800

    init composer test

---
+++ src/Flower/Lily.php
@@ -0,0 +1,15 @@
+<?php
+namespace Flower\Lily;
+
+/**
+ * Class 类名
+ * 类功能
+ *
+ * @version  : 1.0.0
+ * @datetime : 2019/5/29 23:44 23
+ */
+class Lily {
+    public function desc() {
+        echo "this is lily flower";
+    }
+}
---
+++ src/Flower/Rose.php
@@ -0,0 +1,15 @@
+<?php
+namespace Flower\Rose;
+
+/**
+ * Class 类名
+ * 类功能
+ *
+ * @version  : 1.0.0
+ * @datetime : 2019/5/29 23:43 23
+ */
+class Rose {
+    public function desc() {
+        echo "this is rose flower";
+    }
+}
